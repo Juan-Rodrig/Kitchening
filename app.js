93596022b0c6a1cce90542d3446e039c8b70d834
Juan-Rodrig <rodriguezvargas.028@gmail.com>
2024-01-24 14:13:12 -0300

    Se agrego estructura de  kitchening

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express = require("express")
+
+const app = express();
+
+const path = require("path");
+
+const port = 3030;
+
+
+app.listen(port, ()=>{
+    console.log(`corriendo http://localhost:${port}`)
+})
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.use(express.static("public"))
